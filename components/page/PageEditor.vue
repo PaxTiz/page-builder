5c59f603e02f35e7e6b9c6da8d13b6ea6afd7e1c
Valentin Cernuta <vcernuta3@gmail.com>
2023-07-26 23:41:12 +0200

    refactor: move editor in it's own component

---
+++ components/page/PageEditor.vue
@@ -0,0 +1,122 @@
+<script lang="ts" setup>
+import { nanoid } from "nanoid";
+import Sidebar from "~/components/layout/Sidebar.vue";
+import Topbar from "~/components/layout/Topbar.vue";
+import PageRenderer from "~/components/page/PageRenderer.vue";
+
+const { blocks, setBlocks } = useBlocks();
+setBlocks([
+  {
+    id: nanoid(),
+    type: "spacer",
+    name: "Spacer",
+    height: 64,
+  },
+  {
+    id: nanoid(),
+    name: "Container",
+    type: "container",
+    children: [
+      {
+        id: nanoid(),
+        type: "button",
+        name: "Button",
+        text: "Contact me",
+        url: "https://google.fr",
+      },
+      {
+        id: nanoid(),
+        type: "spacer",
+        name: "Spacer",
+        height: 16,
+      },
+      {
+        id: nanoid(),
+        name: "Carousel",
+        type: "carousel",
+        slidesPerPage: 2,
+        slideHeight: 200,
+        navigation: true,
+        pagination: true,
+        children: [
+          {
+            id: nanoid(),
+            name: "Slide #1",
+            type: "carouselItem",
+            title: "My slide",
+            description: "This is a slide description",
+            image:
+              "https://img.freepik.com/free-vector/realistic-horizontal-cinema-movie-time-poster-with-3d-glasses-snacks-tickets-clapper-reel-blue-background-with-bokeh-vector-illustration_1284-77013.jpg",
+            url: "",
+          },
+          {
+            id: nanoid(),
+            name: "Slide #2",
+            type: "carouselItem",
+            title: "My slide",
+            description: "This is a slide description",
+            image:
+              "https://img.freepik.com/free-vector/realistic-horizontal-cinema-movie-time-poster-with-3d-glasses-snacks-tickets-clapper-reel-blue-background-with-bokeh-vector-illustration_1284-77013.jpg",
+            url: "",
+          },
+          {
+            id: nanoid(),
+            name: "Slide #3",
+            type: "carouselItem",
+            title: "My slide",
+            description: "This is a slide description",
+            image:
+              "https://img.freepik.com/free-vector/realistic-horizontal-cinema-movie-time-poster-with-3d-glasses-snacks-tickets-clapper-reel-blue-background-with-bokeh-vector-illustration_1284-77013.jpg",
+            url: "",
+          },
+          {
+            id: nanoid(),
+            name: "Slide #4",
+            type: "carouselItem",
+            title: "My slide",
+            description: "This is a slide description",
+            image:
+              "https://img.freepik.com/free-vector/realistic-horizontal-cinema-movie-time-poster-with-3d-glasses-snacks-tickets-clapper-reel-blue-background-with-bokeh-vector-illustration_1284-77013.jpg",
+            url: "",
+          },
+        ],
+      },
+      {
+        id: nanoid(),
+        type: "spacer",
+        name: "Spacer",
+        height: 16,
+      },
+      {
+        id: nanoid(),
+        type: "cta",
+        name: "Call to action",
+        title: "Interested by our services ?",
+        subtitle: "Contact us to discover what we can do for you",
+        buttonText: "Contact Us",
+        url: "",
+      },
+    ],
+  },
+]);
+</script>
+
+<template>
+  <div class="bg-gray-800">
+    <Sidebar :children="blocks" />
+    <Topbar />
+
+    <div class="page pt-20 z-0 px-8">
+      <div class="shadow-white bg-gray-900 rounded">
+        <PageRenderer :children="blocks" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.page {
+  margin-left: 350px;
+  width: calc(100% - 350px);
+}
+</style>
