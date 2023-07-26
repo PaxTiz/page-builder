0db28d164b7ad097c01991772bc0b7928cd48b13
Valentin Cernuta <vcernuta3@gmail.com>
2023-07-27 00:13:46 +0200

    feat: disable editor on small screens

--- app.vue
+++ app.vue
@@ -10,7 +10,37 @@ const { state } = useApplication();
   <div>
     <Toast />
 
-    <PageEditor v-if="state === 'editor'" />
-    <PagePreview v-if="state === 'preview'" />
+    <div class="application">
+      <PageEditor v-if="state === 'editor'" />
+      <PagePreview v-if="state === 'preview'" />
+    </div>
+
+    <div class="unsupported">
+      <h1 class="mt-0">Your device is not supported</h1>
+      <p class="m-0">The page editor is only available on desktop.</p>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.unsupported {
+  display: none;
+}
+
+@media screen and (max-width: 1120px) {
+  .application {
+    display: none;
+  }
+
+  .unsupported {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    color: #fff;
+    text-align: center;
+    margin: 0 2em;
+  }
+}
+</style>
