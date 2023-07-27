68fbeaa458dc6f111a5cc346daa6471c453425db
Valentin Cernuta <vcernuta3@gmail.com>
2023-07-27 18:44:50 +0200

    fix: button instead of div for pagination items

--- components/layout/Pagination.vue
+++ components/layout/Pagination.vue
@@ -10,9 +10,9 @@ defineProps<{
 
 <template>
   <div class="flex justify-center gap-3">
-    <div
+    <button
       class="pagination__item"
-      :class="{ disabled: modelValue === 1 }"
+      :disabled="modelValue === 1"
       @click="$emit('update:modelValue', modelValue - 1)"
     >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -20,8 +20,8 @@ defineProps<{
           d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"
         ></path>
       </svg>
-    </div>
-    <div
+    </button>
+    <button
       v-for="i in total"
       :key="i"
       class="pagination__item"
@@ -29,10 +29,10 @@ defineProps<{
       @click="$emit('update:modelValue', i)"
     >
       {{ i }}
-    </div>
-    <div
+    </button>
+    <button
       class="pagination__item"
-      :class="{ disabled: modelValue === total }"
+      :disabled="modelValue === total"
       @click="$emit('update:modelValue', modelValue + 1)"
     >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -40,29 +40,29 @@ defineProps<{
           d="M13.1714 12.0007L8.22168 7.05093L9.63589 5.63672L15.9999 12.0007L9.63589 18.3646L8.22168 16.9504L13.1714 12.0007Z"
         ></path>
       </svg>
-    </div>
+    </button>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .pagination__item {
   user-select: none;
-  @apply flex justify-center items-center w-[24px] h-[24px] text-gray-300 hover:text-white text-xs bg-gray-800 hover:bg-gray-900 shadow-white-small rounded cursor-pointer;
+  @apply flex justify-center items-center w-[24px] h-[24px] text-gray-300 hover:text-white text-xs border-none bg-gray-800 hover:bg-gray-900 shadow-white-small rounded cursor-pointer;
 
   svg {
     @apply fill-gray-300 w-[16px] h-[16px];
+  }
+
+  &[disabled] {
+    @apply bg-gray-700 cursor-not-allowed;
+
+    svg {
+      @apply fill-gray-500;
+    }
   }
 }
 
 .active {
   @apply bg-gray-900 shadow-blue text-white font-bold;
-}
-
-.disabled {
-  @apply bg-gray-700 cursor-not-allowed;
-
-  svg {
-    @apply fill-gray-500;
-  }
 }
 </style>
